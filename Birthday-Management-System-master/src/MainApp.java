b877676a8d13581791d9120b3d14a8bab07928f9
PIASH213 <abdulhamidpiash2003@gmail.com>
2025-06-14 13:12:02 +0600

    Update MainApp.java

--- Birthday-Management-System-master/src/MainApp.java
+++ Birthday-Management-System-master/src/MainApp.java
@@ -1,15 +1,32 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
-        BirthdayController controller = new BirthdayController();
-        Scene scene = new Scene(controller.getView(), 600, 500);
-        primaryStage.setTitle("Birthday Management System");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            BirthdayController controller = new BirthdayController();
+            Scene scene = new Scene(controller.getView(), 600, 500);
+            primaryStage.setTitle("Birthday Management System");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            showError("Application Failed to Start", e);
+            System.exit(1);
+        }
+    }
+
+    private void showError(String header, Exception e) {
+        System.err.println(header + ": " + e.getMessage());
+        e.printStackTrace();
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Fatal Error");
+        alert.setHeaderText(header);
+        alert.setContentText(e.getMessage());
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
